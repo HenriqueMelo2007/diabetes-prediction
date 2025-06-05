704f8db151817d4eefbe2c3832f4b557f0aa549e
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-06-05 11:25:51 -0300

    feat: it adds my api

--- back/main.py
+++ back/main.py
@@ -1,16 +1,44 @@
-from typing import Union
-
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+import joblib
+import numpy
+
+model = joblib.load("../mlmodel/ml_model_random_forest_diabetes.pkl")
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class BodyRequest(BaseModel):
+    Pregnancies: int = Field(title="Number of times pregnant")
+    Glucose: int = Field(
+        title="Plasma glucose concentration a 2 hours in an oral glucose tolerance test"
+    )
+    BloodPressure: int = Field(title="Diastolic blood pressure (mm Hg)")
+    SkinThickness: int = Field(title="Triceps skin fold thickness (mm)")
+    Insulin: int = Field(title="2-Hour serum insulin (mu U/ml)")
+    BMI: float = Field(title="Body mass index (weight in kg/(height in m)^2)")
+    DiabetesPedigreeFunction: float = Field(title="Diabetes pedigree function")
+    Age: int = Field(gt=0, title="Age (years)")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/predict")
+def prediction(data: BodyRequest):
+    numpy_data_array = numpy.array(
+        [
+            [
+                data.Pregnancies,
+                data.Glucose,
+                data.BloodPressure,
+                data.SkinThickness,
+                data.Insulin,
+                data.BMI,
+                data.DiabetesPedigreeFunction,
+                data.Age,
+            ]
+        ]
+    )
+
+    prediction = model.predict(numpy_data_array)
+
+    return {"prediction": int(prediction[0])}
